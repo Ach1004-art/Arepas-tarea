55796af1afcf861fe743578c21b549a3188fe7c9
Ach1004-art <a.conejero@correo.unimet.edu.ve>
2024-09-17 07:10:35 -0400

    Add files via upload

---
+++ arepas.py
@@ -0,0 +1,19 @@
+#se declaran los ingredientes 
+agua = int(input("cuantas tazas de agua?")) #El agua se da en tazas 
+print("el agua es",agua)
+harina = 1 #la harina se da en tazas 
+print("la harina es",harina)
+sal = 1/16/3 #La sal se convierte de cdtas a tazas, hay 3 cdtas por por cda y 16 cdas por taza 
+print("la sal", sal)
+aceite = 1/16 #El aceite se convierte decdas a tazas 
+
+#comienza la preparacion de la arepa 
+bol = agua + harina + sal 
+budare = bol + aceite 
+
+#se imprime el resultado 
+print("La areoa final es",budare)
+
+input("presione enter para continuar")
+
+  
